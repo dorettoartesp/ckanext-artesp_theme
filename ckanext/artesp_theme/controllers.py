f0a7531c35c7304ae58dcfe5818867961aa43ddc
Doretto <dorettoartesp@gmail.com>
2025-05-20 16:42:33 -0300

    feat(stats): redirect non-logged users to login page

--- ckanext/artesp_theme/controllers.py
+++ ckanext/artesp_theme/controllers.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, g
+from ckan.plugins import toolkit
+from ckan.lib.helpers import flash_error, redirect_to
 
 artesp_theme = Blueprint('artesp_theme', __name__)
 
@@ -23,3 +25,14 @@ artesp_theme.add_url_rule('/about-ckan', view_func=about_ckan)
 artesp_theme.add_url_rule('/terms', view_func=terms)
 artesp_theme.add_url_rule('/privacy', view_func=privacy)
 artesp_theme.add_url_rule('/contact', view_func=contact)
+
+@artesp_theme.before_app_request
+def restrict_stats_page_access():
+    """
+    Restricts access to the /stats page to authenticated users.
+    """
+    if request.path == '/stats':
+        if not g.user:  # g.user is the username, empty for anonymous
+            flash_error(toolkit._('You must be logged in to access the statistics page.'))
+            return redirect_to(toolkit.url_for('user.login'))
+    return None # Continue processing the request
